9196833e844ddc060f8eebd9b45d5780356da294
anipbhattacharya <anipbhattacharya@gmail.com>
2025-07-27 09:32:47 +0000

    Add solution for Remove Outermost Parentheses

---
+++ STRINGS/outermost_parenthesis.java
@@ -0,0 +1,32 @@
+public class outermost_parenthesis{
+    
+    public static String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int level = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                if (level > 0) {
+                    result.append(c);
+                }
+                level++;
+            } else { // c == ')'
+                level--;
+                if (level > 0) {
+                    result.append(c);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "(()())(())(()(()))";
+        String output = removeOuterParentheses(input);
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + output);
+    }
+}
